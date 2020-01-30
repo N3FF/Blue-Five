a122190e60e0b0943f888d011808644376509869
N3FF <neffj@uw.edu>
2020-01-29 20:41:49 -0800

    Fixxed Keypresses

--- main.js
+++ main.js
@@ -37,11 +37,11 @@ Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
     var locY = y;
     var offset = vindex === 0 ? this.startX : 0;
     ctx.drawImage(this.spriteSheet,
-                  index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
-                  this.frameWidth, this.frameHeight,
-                  locX, locY,
-                  this.frameWidth * scaleBy,
-                  this.frameHeight * scaleBy);
+        index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
+        this.frameWidth, this.frameHeight,
+        locX, locY,
+        this.frameWidth * scaleBy,
+        this.frameHeight * scaleBy);
 }
 
 Animation.prototype.currentFrame = function () {
@@ -52,13 +52,12 @@ Animation.prototype.isDone = function () {
     return (this.elapsedTime >= this.totalTime);
 }
 
-function Background(game) 
-{
-	//this.img = document.getElementById("Background");
-	this.back1 = new Animation(ASSET_MANAGER.getAsset("./img/Background.png"), 0, 0, 1680, 1050, 1, 1, true, true);
-	//this.tile1 = new Animation(ASSET_MANAGER.getAsset("./img/52Tile.png"), 0, 0, 52, 52, 1, 1, true, true);
-	this.tile1 = new Animation(ASSET_MANAGER.getAsset("./img/52Tilea.png"), 0, 0, 52, 52, 1, 1, true, true);
-	this.hud = new Animation(ASSET_MANAGER.getAsset("./img/HudPrototype1.png"), 0, 0, 250, 360, 1, 1, true, true);
+function Background(game) {
+    //this.img = document.getElementById("Background");
+    this.back1 = new Animation(ASSET_MANAGER.getAsset("./img/Background.png"), 0, 0, 1680, 1050, 1, 1, true, true);
+    //this.tile1 = new Animation(ASSET_MANAGER.getAsset("./img/52Tile.png"), 0, 0, 52, 52, 1, 1, true, true);
+    this.tile1 = new Animation(ASSET_MANAGER.getAsset("./img/52Tilea.png"), 0, 0, 52, 52, 1, 1, true, true);
+    this.hud = new Animation(ASSET_MANAGER.getAsset("./img/HudPrototype1.png"), 0, 0, 250, 360, 1, 1, true, true);
     Entity.call(this, game, 0, 400);
     this.radius = 200;
 }
@@ -73,18 +72,18 @@ Background.prototype.draw = function (ctx) {
     ctx.fillStyle = "#808080";
     //ctx.drawImage(this.idlrL, 10, 10);
     //ctx.fillRect(0,500,1000,300);  
-	this.back1.drawFrame(this.game.clockTick, ctx, 0, 0, 1);
-	//ctx.fillRect(0,500,1000,300);
-	
-	var tileSize = 52;
-	for(i = 0; i < 20; i++) {
-		for(j = 0; j < 3; j++) {
-			this.tile1.drawFrame(this.game.clockTick, ctx, i * tileSize, (600 + j * tileSize), 1);
-		}
-		
-	}
-	
-	this.hud.drawFrame(this.game.clockTick, ctx, 875, 0, 1/2);
+    this.back1.drawFrame(this.game.clockTick, ctx, 0, 0, 1);
+    //ctx.fillRect(0,500,1000,300);
+
+    var tileSize = 52;
+    for (i = 0; i < 20; i++) {
+        for (j = 0; j < 3; j++) {
+            this.tile1.drawFrame(this.game.clockTick, ctx, i * tileSize, (600 + j * tileSize), 1);
+        }
+
+    }
+
+    this.hud.drawFrame(this.game.clockTick, ctx, 875, 0, 1 / 2);
     Entity.prototype.draw.call(this);
 }
 
@@ -114,114 +113,107 @@ Hero.prototype.constructor = Hero;
 
 // Having the unicorn jump here
 Hero.prototype.update = function () {
-	
-	if (this.y > this.ground) {
-		this.jumping = false;
-		this.y = this.ground;
-	    this.canJump = true;
-	    this.yAccel = 0;
-	}
-	
-	
-	if (this.jumping === false) {
-		if (this.accel < -1) {
-			this.accel += .2;
-		} else if (this.accel > 1) {
-			this.accel -= .2;
-		} else {
-			this.accel = 0;
-		}
-	}
-	
-	
-	this.x = this.x + this.accel;
-	this.y = this.y + this.yAccel;
-	
-	
-    if (this.game.space) {
-    	this.jumping = true;
+
+    if (this.y > this.ground) {
+        this.jumping = false;
+        this.y = this.ground;
+        this.canJump = true;
+        this.yAccel = 0;
     }
-    
-    if (this.game.right) {
-    	this.moveR = true;
-    } else {
-    	this.moveR = false;
-    }
-    
-    if (this.game.left) {
-    	this.moveL = true;
-    } else {
-    	this.moveL = false;
-    }
-    
-    
-    if (this.jumping) {
-    	if (this.canJump) {
-     	   this.yAccel = -25;
-        }
-       this.canJump = false;
-       
-       this.yAccel = this.yAccel + this.gravity;
-       
-    }
-    
-    if (this.moveR) {
-    	this.direction = true;
-     if (this.accel > 0) {
-    	 this.accel = 10;
-    	 
-     } else {
-    	 this.accel = 5;
-     }
-     //this.x = this.x + this.accel;
-     //this.moveR = false;
-    }
-    
-    if (this.moveL) {
-    	this.direction = false;
-    	if (this.accel < 0) {
-       	 this.accel = -10; 
+
+
+    if (this.jumping === false) {
+        if (this.accel < -1) {
+            this.accel += .2;
+        } else if (this.accel > 1) {
+            this.accel -= .2;
         } else {
-        	this.accel = -5;
+            this.accel = 0;
+        }
+    }
+
+
+    this.x = this.x + this.accel;
+    this.y = this.y + this.yAccel;
+
+
+    if (!this.jumping && this.game.keysActive[' '.charCodeAt(0)]) {
+        this.jumping = true;
+    }
+
+    this.moveR = this.game.keysActive['D'.charCodeAt(0)] ||
+        this.game.keysActive[39]; //39 = Left arrow key code
+    this.moveL = this.game.keysActive['A'.charCodeAt(0)] ||
+        this.game.keysActive[37]; //39 = Right arrow key code
+
+
+    if (this.jumping) {
+        if (this.canJump) {
+            this.yAccel = -25;
+        }
+        this.canJump = false;
+
+        this.yAccel = this.yAccel + this.gravity;
+
+    }
+
+    if (this.moveR) {
+        this.direction = true;
+        if (this.accel > 0) {
+            this.accel = 10;
+
+        } else {
+            this.accel = 5;
+        }
+        //this.x = this.x + this.accel;
+        //this.moveR = false;
+    }
+
+    if (this.moveL) {
+        this.direction = false;
+        if (this.accel < 0) {
+            this.accel = -10;
+        } else {
+            this.accel = -5;
         }
         //this.x = this.x + this.accel;
         //this.moveL = false;
-       }
-    
-    
-    
-    
+    }
+
+
+
+
     Entity.prototype.update.call(this);
 }
 
 Hero.prototype.draw = function (ctx) {
     if (this.jumping) {
-    	
-    	if (this.direction) {
-    		 this.jumpAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
-    	} else {
-    		this.jumpAnimationL.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
-    	}
-       
-    	
-    } 
-    
-    
+
+        if (this.direction) {
+            this.jumpAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
+        } else {
+            this.jumpAnimationL.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
+        }
+
+
+    }
+
+
     else if (this.accel != 0) {
-    	if (this.direction) {
-    		this.RunningR.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
-   	} else {
-   		   this.RunningL.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
-   	}
-      
-    }  else {
-    	
-    	if (this.direction) {
-    		this.idleR.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
-    	} else {
-    		this.idleL.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
-    	}
-        
+        if (this.direction) {
+            this.RunningR.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
+        } else {
+            this.RunningL.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
+        }
+
+    } else {
+
+        if (this.direction) {
+            this.idleR.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
+        } else {
+            this.idleL.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
+        }
+
     }
     Entity.prototype.draw.call(this);
 }
@@ -246,7 +238,7 @@ ASSET_MANAGER.downloadAll(function () {
 
     gameEngine.addEntity(bg);
     gameEngine.addEntity(hero);
- 
+
     gameEngine.init(ctx);
     gameEngine.start();
 });
