791ebc40ce8daace31e41f36d3831848eaaf0ddc
N3FF <neffj@uw.edu>
2020-02-04 14:25:06 -0800

    Commented function parameters.

--- physics.js
+++ physics.js
@@ -1,3 +1,16 @@
+/**
+ * 
+ * @param {number} startX       Object X position
+ * @param {number} startY       Object Y position
+ * @param {number} duration     Number of ticks before the object disappears
+ * @param {number} cursorX      Cursor X position  
+ * @param {number} cursorY      Cursor Y position
+ * @param {number} gravity      Gravitational force
+ * @param {number} velocity     Initial velocity
+ * @param {number} acceleration Acceleration over time
+ * 
+ * @description Attach physics properties to an object
+ */
 function Physics(startX, startY, duration, cursorX, cursorY, gravity, velocity, acceleration) {
     // console.log(startX, startY);
     this.velocity = Math.ceil(velocity);
@@ -21,6 +34,9 @@ function Physics(startX, startY, duration, cursorX, cursorY, gravity, velocity, 
     //console.log(this.x, this.cursorX, this.y, this.cursorY, this.angle);
 }
 
+/**
+ * @description Advance the time value by +1
+ */
 Physics.prototype.tick = function () {
     this.time += 1;
     this.projectile();
@@ -29,6 +45,9 @@ Physics.prototype.tick = function () {
     }
 }
 
+/**
+ * @description Updates the X and Y position
+ */
 Physics.prototype.projectile = function () {
     if (this.gravity != 0) {
         this.y = this.startY + Math.sin(this.angle) * this.velocity * this.time + .5 * this.gravity * (this.time * this.time);
@@ -44,13 +63,24 @@ Physics.prototype.projectile = function () {
     this.x = Math.ceil(this.x);
 }
 
+/**
+ * @param {boolean} bounces
+ * @param {boolean} sticks
+ * 
+ * @description Defines the action that occurs when the object collides with another.
+ */
 Physics.prototype.collision = function (bounces, sticks) {
 
 }
+/**
+ * @param {Number} gravity - Graviational Constant in pixels per second
+ */
 Physics.prototype.setGravity = function (gravity) {
     this.gravity = gravity;
 }
-
+/**
+ * @param {String} type - Angle type "rad" (radians) or "deg" (degrees)
+ */
 Physics.prototype.getAngle = function (type = "rad") {
     var deltaX = this.cursorX - this.startX;
     var deltaY = this.cursorY - this.startY;
@@ -58,10 +88,16 @@ Physics.prototype.getAngle = function (type = "rad") {
     return type === "deg" ? angle * Math.PI / 180.0 : angle;
 }
 
+/**
+ * @returns {Object} Object with x , y coordinates
+ */
 Physics.prototype.getPosition = function () {
     return { "x": this.x, "y": this.y };
 }
 
+/**
+ * @return {boolean} true if the object has achieved it's goal (timeout, collision, etc.)
+ */
 Physics.prototype.isDone = function () {
-    return this.dead || (this.time === this.endTime);
+    return this.dead;
 }
