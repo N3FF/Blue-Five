405bbfbcaf34ae4b2b227929d1f21c34fde67906
Jake M <jacobmarquardt1@gmail.com>
2020-02-14 10:35:55 -0800

    Added hp bar files and removed hud prototype

--- environment.js
+++ environment.js
@@ -1,7 +1,6 @@
 function Background(game) {
     this.back1 = new Animation(ASSET_MANAGER.getAsset("./img/environment/Background.png"), 0, 0, 1680, 1050, 1, 1, true, true);
     this.tile1 = new Animation(ASSET_MANAGER.getAsset("./img/environment/52Tilea.png"), 0, 0, 52, 52, 1, 1, true, true);
-    this.hud = new Animation(ASSET_MANAGER.getAsset("./img/HudPrototype1.png"), 0, 0, 250, 360, 1, 1, true, true);
 
     this.instructions = new Animation(ASSET_MANAGER.getAsset("./img/Instructions.png"), 0, 0, 370, 202, 1, 1, true, true);
 
@@ -16,18 +15,7 @@ Background.prototype.update = function () {
 }
 
 Background.prototype.draw = function (ctx, xView, yView) {
-    ctx.fillStyle = "#808080";
     this.back1.drawFrame(this.game.clockTick, ctx, -xView, -yView, 1);
-
-//    var tileSize = 52;
-//    for (i = 0; i < 35; i++) {
-//        for (j = 0; j < 3; j++) {
-//            this.tile1.drawFrame(this.game.clockTick, ctx, i * tileSize - xView, (600 + j * tileSize) - yView, 1);
-//        }
-//
-//    }
-
-    this.hud.drawFrame(this.game.clockTick, ctx, 875, 0, 1 / 2);
     this.instructions.drawFrame(this.game.clockTick, ctx, 0, 0, .75);
     Entity.prototype.draw.call(this);
 }
