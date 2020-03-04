a729c89112e7f9e94c95dde9ef19f5d55165ba4e
N3FF <neffj@uw.edu>
2020-03-04 00:01:27 -0800

    Added additional tiles

--- environment.js
+++ environment.js
@@ -1,6 +1,6 @@
 function Background(game) {
     this.back1 = new Animation(ASSET_MANAGER.getAsset("./img/environment/Background.png"), 0, 0, 13824, 1037, 1, 1, true, true);
-    this.tile1 = new Animation(ASSET_MANAGER.getAsset("./img/environment/52Tilea.png"), 0, 0, 52, 52, 1, 1, true, true);
+    this.tile;// = new Animation(ASSET_MANAGER.getAsset("./img/environment/52Tilea.png"), 0, 0, 52, 52, 1, 1, true, true);
     this.win = new Animation(ASSET_MANAGER.getAsset("./img/hud/Win.png"), 0, 0, 370, 202, 1, 1, true, true);
 
     Entity.call(this, game, 0, 400);
@@ -14,34 +14,62 @@ Background.prototype.update = function () {
 }
 
 Background.prototype.draw = function (ctx, xView, yView) {
-	var hero = this.game.entities[1];
-	
+    var hero = this.game.entities[1];
+
     this.back1.drawFrame(this.game.clockTick, ctx, -xView, -yView, 1);
-    
+
     if (hero.win) {
-    	this.win.drawFrame(this.game.clockTick, ctx, ctx.canvas.width - 370 * .75, 0, .75);
-    } 
-    
+        this.win.drawFrame(this.game.clockTick, ctx, ctx.canvas.width - 370 * .75, 0, .75);
+    }
+
     Entity.prototype.draw.call(this);
 }
 
 function Platform(game, x, y, type, win) {
-	
-	if( type === 1) {
-    this.Tile1 = new Animation(ASSET_MANAGER.getAsset("./img/environment/52Tilea.png"), 0, 0, 52, 52, .20, 1, true, true);
-	} else {
-		this.Tile1 = new Animation(ASSET_MANAGER.getAsset("./img/environment/52Tile.png"), 0, 0, 52, 52, .20, 1, true, true);
-	}
-	
+
+
     this.radius = 52;
     this.width = 52;
-    this.height = 52;  
+    this.height = 52;
     this.type = TYPES.PLATFORM;
     this.win = true;
+    this.fileName = "./img/environment/";
+
+    switch (type) {
+        case "invisible":
+            this.fileName += "invisible.png";
+            break;
+
+        case "floating_spikes":
+            this.fileName += "floating_spikes.png";
+            break;
+
+        case "floor_spikes":
+            this.fileName += "floor_spikes.png";
+            break;
+
+        case "gap_right":
+            this.fileName += "floor_gap_right.png";
+            break;
+
+        case "gap_left":
+            this.fileName += "floor_gap_left.png";
+            break;
+
+        case "floor":
+            this.fileName += "floor.png";
+            break;
+
+        case "steel_block":
+            this.fileName += "steel_block.png";
+            break;
+    }
     
+    this.tile = new Animation(ASSET_MANAGER.getAsset(this.fileName), 0, 0, this.width, this.height, .20, 1, true, true);
+
     // For future
     this.walkableTerrain = false;
-    
+
     Entity.call(this, game, x, y);
 }
 
@@ -50,13 +78,13 @@ Platform.prototype.constructor = Platform;
 
 // The update function
 Platform.prototype.update = function () {
- 
+
     Entity.prototype.update.call(this);
 }
 
 Platform.prototype.draw = function (ctx, xView, yView) {
-	
-	this.Tile1.drawFrame(this.game.clockTick, ctx, this.x - xView, this.y - yView, 1);
+
+    this.tile.drawFrame(this.game.clockTick, ctx, this.x - xView, this.y - yView, 1);
 
     Entity.prototype.draw.call(this);
 }
